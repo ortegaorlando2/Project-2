d5f9c01b2aad950bd3f1d66357ac22f922469485
Dana <35933787+danawoodruff@users.noreply.github.com>
2021-03-23 13:32:08 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,9 +2,14 @@
 // Store our API endpoint inside queryUrl
 let queryUrl = "static/data/final_HAR_df.geojson";
 
+// Helper function to add commas to List Price popup
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(data => {
-    
+
     // Sending our homes layer to the createMap function
     createMap();
 
@@ -33,6 +38,57 @@ d3.json(queryUrl).then(data => {
             "Light Map": lightmap
         };
 
+        //Attribution: "https://www.flaticon.com/authors/becris" 
+        // Custom markers
+        var homeIcon = L.icon({
+            iconUrl: 'Images/home.svg',
+            
+            iconSize:     [15, 15], // size of the icon
+            iconAnchor:   [7, 7], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+        }); 
+
+        var pinkIcon = L.icon({
+            iconUrl: 'Images/pink.png',
+            
+            iconSize:     [15, 15], // size of the icon
+            iconAnchor:   [7, 7], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+        }); 
+
+        var greenIcon = L.icon({
+            iconUrl: 'Images/green.png',
+            
+            iconSize:     [15, 15], // size of the icon
+            iconAnchor:   [7, 7], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+        }); 
+
+        var purpleIcon = L.icon({
+            iconUrl: 'Images/purple.png',
+            
+            iconSize:     [15, 15], // size of the icon
+            iconAnchor:   [7, 7], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+        });
+        
+        var yellowIcon = L.icon({
+            iconUrl: 'Images/yellow.png',
+            
+            iconSize:     [15, 15], // size of the icon
+            iconAnchor:   [7, 7], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+        });
+
+        var blackIcon = L.icon({
+            iconUrl: 'Images/black.png',
+            
+            iconSize:     [15, 15], // size of the icon
+            iconAnchor:   [7, 7], // point of the icon which will correspond to marker's location
+            popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+        });
+
+
         // Create an object to keep track of active layers and each layer with its markers
         const layers = {
             active: [],
@@ -40,14 +96,26 @@ d3.json(queryUrl).then(data => {
             two: new L.LayerGroup(),
             three: new L.LayerGroup(),
             four: new L.LayerGroup(),
-            five: new L.LayerGroup()
+            five: new L.LayerGroup(),
+            six: new L.LayerGroup()
         };
+
+        const choro = {
+            six: new L.LayerGroup()
+        };
+
+        
 
         // Initialize all of the LayerGroups
         layers.one = L.geoJson(data, {
             filter: function (feature, layer) {
                 return (feature.properties.list_price <= 250000);
             },
+
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: pinkIcon});
+            },
+
             // Creates popup
             onEachFeature: function (feature, layer) {
                 // console.log(layer)  // To test for function operation
@@ -56,7 +124,7 @@ d3.json(queryUrl).then(data => {
                     feature.properties.full_address +
                     '</h3><hr><p>' +
                     '$' +
-                    feature.properties.list_price
+                    numberWithCommas(feature.properties.list_price)
                 ) + '</p>'
             }
         })
@@ -68,13 +136,18 @@ d3.json(queryUrl).then(data => {
                     feature.properties.list_price <= 500000
                 )
             },
+
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: yellowIcon});
+            },
+
             onEachFeature: function (feature, layer) {
                 layer.bindPopup(
                     '<h3>' +
                     feature.properties.full_address +
                     '</h3><hr><p>' +
                     '$' +
-                    feature.properties.list_price
+                    numberWithCommas(feature.properties.list_price)
                 ) + '</p>'
             }
         })
@@ -86,13 +159,18 @@ d3.json(queryUrl).then(data => {
                     feature.properties.list_price <= 750000
                 )
             },
+
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: greenIcon});
+            },
+
             onEachFeature: function (feature, layer) {
                 layer.bindPopup(
                     '<h3>' +
                     feature.properties.full_address +
                     '</h3><hr><p>' +
                     '$' +
-                    feature.properties.list_price
+                    numberWithCommas(feature.properties.list_price)
                 ) + '</p>'
             }
         })
@@ -104,13 +182,18 @@ d3.json(queryUrl).then(data => {
                     feature.properties.list_price <= 1000000
                 )
             },
+
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: purpleIcon});
+            },
+
             onEachFeature: function (feature, layer) {
                 layer.bindPopup(
                     '<h3>' +
                     feature.properties.full_address +
                     '</h3><hr><p>' +
                     '$' +
-                    feature.properties.list_price
+                    numberWithCommas(feature.properties.list_price)
                 ) + '</p>'
             }
         })
@@ -119,48 +202,195 @@ d3.json(queryUrl).then(data => {
             filter: function (feature, layer) {
                 return (feature.properties.list_price >= 1000000);
             },
+
+            pointToLayer: function(feature, latlng) {
+                return L.marker(latlng, {icon: blackIcon});
+            },
+
             onEachFeature: function (feature, layer) {
                 layer.bindPopup(
                     '<h3>' +
                     feature.properties.full_address +
                     '</h3><hr><p>' +
                     '$' +
-                    feature.properties.list_price
+                    numberWithCommas(feature.properties.list_price)
                 ) + '</p>'
             }
         })
 
-// Create an overlays object to add to the layer control
-let overlayMaps = {
-    "Up to $250K": layers.one,
-    "$250K - $500K": layers.two,
-    "$500K - $750K": layers.three,
-    "$750K - $1M": layers.four,
-    "Over $1M": layers.five
+        //********************************************************************************** */
+    //     // Load in geojson data
+    //     let geoData = "static/data/elem_heatmap.geojson";
+
+    //     let lat = features.geometry.coordinates[1];
+    //     let long = features.geometry.coordinates[0]
+
+    //     // Grab data with d3  //https://www.technbuzz.com/2020/06/28/add-leaflet-markers-popup-and-circle/
+    //     d3.json(geoData).then(function schoolMap() {
+    //         let circleA = L.circle([lat, long], { //is lat,long in curly brackets?
+    //             color: "green",
+    //             fillColor: "green",
+    //             fillOpacity: 0.5,
+    //             radius: 100
+    //         })
+    //         let circleB = L.circle([lat, long], {
+    //             color: "yellow",
+    //             fillColor: "yellow",
+    //             fillOpacity: 0.5,
+    //             radius: 100
+    //         })
+    //         let circleC = L.circle([lat, long], {
+    //             color: "orange",
+    //             fillColor: "orange",
+    //             fillOpacity: 0.5,
+    //             radius: 100
+    //         })
+    //         let circleD = L.circle([lat, long], {
+    //             color: "pink",
+    //             fillColor: "pink",
+    //             fillOpacity: 0.5,
+    //             radius: 100
+    //         })
+    //         let circleF = L.circle([lat, long], {
+    //             color: "red",
+    //             fillColor: "red",
+    //             fillOpacity: 0.5,
+    //             radius: 100
+    //         })
+
+    //     let home = features.geometry.coordinates;
+    //     home.forEach( (home,i) => {
+    //         //"A" rated elementary school
+    //         if (features.properties.rating = "A") {
+    //             pointToLayer: function (feature, latlong) {
+    //                 return L.circleMarker(latlong, circleA);
+    //             }}
+    //         //"B" rated elementary school
+    //         else if (features.properties.rating = "B") {
+    //                 pointToLayer: function (feature, latlong) {
+    //                     return L.circleMarker(latlong, circleB);
+    //                 }}
+    //         //"C" rated elementary school
+    //         else if (features.properties.rating = "C") {
+    //                     pointToLayer: function (feature, latlong) {
+    //                         return L.circleMarker(latlong, circleC);
+    //                     }}
+    //         //"D" rated elementary school
+    //         else if (features.properties.rating = "D") {
+    //                         pointToLayer: function (feature, latlong) {
+    //                             return L.circleMarker(latlong, circleD);
+    //                         }}
+    //         //"F" rated elementary school
+    //         else (features.properties.rating = "F") {
+    //                             pointToLayer: function (feature, latlong) {
+    //                                 return L.circleMarker(latlong, circleF);
+    //                             }}
+
+    //         })
+    // }
+    //**************************************************************************** */   
+    // Load in geojson data
+    // let geoData = "static/data/elem_heatmap.geojson";
+
+    // function getColor(d) {
+    //     return d = 'A' ? '#800026' :
+    //         d = 'B'  ? '#BD0026' :
+    //         d = 'C'  ? '#E31A1C' :
+    //         d = 'D'  ? '#FC4E2A' :
+    //         d = 'F'   ? '#FD8D3C' :
+    //                     '#FFEDA0';
+    // }
+    // let geojson;
+
+    //features.properties.rating
+    //features.geometry.coordinates[0] and [1]
+
+    // Grab data with d3
+    // d3.json(geoData).then(function (schoolData) {
+
+    //     // Create a new choropleth layer
+    //     choro.six = L.choropleth(schoolData, {
+
+    //         // Define what  property in the features to use
+    //         valueProperty: "rating",
+
+    //         // Set color scale
+    //         scale: ["#ffffb2", "#b10026"],
+
+    //         // Number of breaks in step range
+    //         steps: 5,
+
+    //         // Style
+    //         mode: "q",
+    //         style: {
+    //             // Border color
+    //             color: "#fff",
+    //             weight: .8,
+    //             fillOpacity: 0.5
+    //         }
+    //     })
+    //************************************************************************** */
+    // // Load in geojson data
+    // let geoData = "static/data/elem_heatmap.geojson";
+
+    // function getColor(d) {
+    //     return d = 'A' ? '#800026' :
+    //         d = 'B'  ? '#BD0026' :
+    //         d = 'C'  ? '#E31A1C' :
+    //         d = 'D'  ? '#FC4E2A' :
+    //         d = 'F'   ? '#FD8D3C' :
+    //                     '#FFEDA0';
+    // }
+
+    // choro.six = L.geoJson(geoData, {
+    //     style: function (feature) {
+    //         return {
+    //             "color": getColor(feature.properties.rating),
+    //             "opacity": 1,
+    //         }
+    //     }
+    // })
+
+
+
+
+    //*********************************************************************************** */
+    // Create an overlays object to add to the layer control
+    let overlayMaps = {
+        "Up to $250K": layers.one,
+        "$250K - $500K": layers.two,
+        "$500K - $750K": layers.three,
+        "$750K - $1M": layers.four,
+        "Over $1M": layers.five,
+        "School Quality": choro.six
+    };
+
+    // let schoolOverlay = {
+    //     "School Quality": chloro.six
+    // };
+
+    // Create our map, giving it the streetmap and homes layers to display on load
+    let myMap = L.map("map", {
+        center: [
+            29.7389278, -95.3651312
+        ],
+        zoom: 12,
+        layers: [
+            streetmap,
+            layers.one,
+            layers.two,
+            layers.three,
+            layers.four,
+            layers.five
+        ]
+    });
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
 };
-
-// Create our map, giving it the streetmap and homes layers to display on load
-let myMap = L.map("map", {
-    center: [
-        29.7389278, -95.3651312
-    ],
-    zoom: 12,
-    layers: [
-        streetmap,
-        layers.one,
-        layers.two,
-        layers.three,
-        layers.four,
-        layers.five
-    ]
-});
-
-// Create a layer control
-// Pass in our baseMaps and overlayMaps
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-}).addTo(myMap);
-}
 
 });
